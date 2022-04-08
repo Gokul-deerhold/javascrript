6aaf2b063c28e22e8a2d768a6cf929d124b74ac8
Gakul sunar <sunaru321@gmail.com>
2022-04-08 17:53:32 +0545

    File changed

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
 
-// console.log('Hello world');
-// var myName='Gakul Sunar';
- //var myAge=29;
-// console.log(myAge);
-//var myName = 'Gakul Sunar';
-//console.log(myName);
-//console.log(typeof(myName));
-//var iAmGakul = true;
-//console.log(iAmGakul);
-//console.log(typeof(iAmGakul));
-//console.log(10+"20");
-//console.log("java script");
-//console.log("" + "")
-
-//console.log (true + true);
-//console.log("Gakul" - "sunar");
-// var iAmUseless=null;
-// console.log(iAmUseless);
-// console.log(typeof(iAmUseless));
-// var iAmStandBy;
-// console.log(iAmStandBy);
-var myPhoneNumber = 9862148169;
-var myName = "Gakul suanr";
-console.log(isNaN(myPhoneNumber ));
-console.log(isNaN(myName));
+ console.log('Hello world');
+ var myName='Gakul Sunar';
+ var myAge=29;
+  console.log(myAge);
