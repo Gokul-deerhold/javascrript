a666478b67cd84089bbad21dd572b003d1e85698
Gakul sunar <sunaru321@gmail.com>
2022-04-18 14:32:33 +0545

    let's start

---
+++ operator.js
@@ -0,0 +1,10 @@
+//var age=20;
+//if(age>=18 && age<= 21){
+ //   consloe.log("yes you are eligible")
+
+//}
+var a =10;
+var b =15;
+if(a>=10 || b<= 20){
+    console.log("yes ypu are eligible")
+}
